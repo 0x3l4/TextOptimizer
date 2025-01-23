ca5a66acac97ece480b9f75c8c702ab9103fad99
Alexander Vereta <alexander.git@yandex.ru>
2025-01-24 02:03:43 +0500

    Initial commit. v.1.0

---
+++ TextOptimizer/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TextOptimizer
+{
+	internal class Program
+	{
+		public static void Main()
+		{
+			string inputFileName = "russian_verbs.txt";
+			string outputFileName = $"filtered_{inputFileName}";
+			string binPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+			string inputFilePath = Path.Combine(binPath, "Input", $"{inputFileName}");
+			string outputFilePath = Path.Combine(binPath, "Output", $"{outputFileName}");
+
+            try
+			{
+				TextOptimizer.OptimizeFile(inputFilePath, outputFilePath);
+			}
+			catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+            }
+		}
+	}
+}
---
+++ TextOptimizer/TextOptimizer.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextOptimizer
+{
+    public static class TextOptimizer
+    {
+        public static void OptimizeFile(string inputFilePath, string outputFilePath)
+        {
+            Console.WriteLine($"Начинаю обработку файла {inputFilePath}...");
+
+            HashSet<string> uniqueWords = new HashSet<string>();
+            int deletedRowsCount = 0;
+            
+            var table = new ConsoleTable("№", "Слово", "Причина удаления", "Строка");
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string? line;
+                int currentLine = 1;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Replace('ё', 'е').Replace('Ё', 'Е');
+
+                    if (!line.Contains('-'))
+                    {
+                        if (!uniqueWords.Add(line.Trim()))
+                        {
+                            deletedRowsCount++;
+                            table.AddRow(deletedRowsCount, line, "Дубликат", currentLine);
+                        }
+                    }
+                    else
+                    {
+                        deletedRowsCount++;
+                        table.AddRow(deletedRowsCount, line, "Содержит тире", currentLine);
+                    }
+
+                    currentLine++;
+                }
+            }
+
+            Console.WriteLine(table);
+
+            File.WriteAllLines(outputFilePath, uniqueWords.ToArray());
+
+            Console.WriteLine($"Обработка завершена. Результат сохранён в файл: {outputFilePath}.");
+            Console.WriteLine($"Было строк: {deletedRowsCount + uniqueWords.Count}.");
+            Console.WriteLine($"Удалено строк: {deletedRowsCount}.");
+            Console.WriteLine($"Итого строк в новом файле: {uniqueWords.Count}.");
+        }
+    }
+}
